15a00a4e1fd8177f0b5baaf7a7501eac5673087a
StahlbrinkM <stahlbrink.marcus@gmail.com>
2022-12-06 18:39:42 +0100

    Small styling improvements

--- src/App.js
+++ src/App.js
@@ -12,16 +12,21 @@ function App() {
   return (
     <div className="App">
       <div className="w-screen bg-blue-300 h-96 flex justify-center items-center">
-        <h1 className="text-6xl text-green-600 border border-black">Rick and Morty Facts!</h1>
+        <h1 className="text-6xl text-green-600 border border-black font-bold">Rick and Morty Facts</h1>
       </div>
-      {storage.map((obj) => {
-        return <div key={obj.id} className='flex gap-2'>
-          <p>Name: {obj.name}</p>
-          <p>Status: {obj.status}</p>
-          <p>Gender: {obj.gender}</p>
-          <img src={obj.image} alt='Character avatar' className="w-24"/>
-        </div>
-      })}
+      <div className="grid grid-rows-3 grid-cols-3 gap-5 m-4">
+        {storage.map((person) => {
+          return <div key={person.id} className='flex gap-2 bg-purple-600 rounded-md'>
+            <div className="p-4">
+              <p>Name: {person.name}</p>
+              <p>Status: {person.status}</p>
+              <p>Gender: {person.gender}</p>
+            </div>
+            <img src={person.image} alt='Character avatar' className="w-36 ml-auto rounded"/>
+          </div>
+        })}
+
+      </div>
 
     </div>
   );
