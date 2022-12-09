7b1915b4dbd148ace5f9d7c34100d58e7fd9e066
StahlbrinkM <marcus_ow@hotmail.com>
2022-12-09 17:54:50 +0100

    Added changeable pages feature

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
 
   const [storage, getStorage] = useState([])
+  const [page, setPage] = useState(1)
 
-  fetch("https://rickandmortyapi.com/api/character")
-    .then(res => res.json())
-    .then(data => getStorage(data.results))
 
+
+    useEffect(() => {
+      fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+      .then(res => res.json())
+      .then(data => getStorage(data.results))
+    }, [page])
+
+ 
   return (
     <div className="App">
       <div className="w-screen bg-blue-300 h-96 flex justify-center items-center">
-        <h1 className="text-6xl text-green-600 border border-black font-bold">Rick and Morty Facts</h1>
+
+        <h1 className="text-6xl text-green-600 font-bold">Rick and Morty Facts</h1>
       </div>
-      <div className="grid grid-rows-3 grid-cols-3 gap-5 m-4">
+      <div className="flex justify-between mx-10 h-20 items-center">
+        <button 
+          className="p-3 border-black border rounded-3xl hover:bg-slate-200 hover:pointer focus:bg-slate-200"
+          onClick={() => {page > 1 ? setPage(prevPage => prevPage - 1) : console.log("XD")}}>
+          Previous Page
+        </button>
+
+        <button 
+          className="p-3 border-black border rounded-3xl hover:bg-slate-200 hover:pointer focus:bg-slate-200"
+          onClick={() => {page < 42 ? setPage(prevPage => prevPage + 1) : console.log("Last page")}}>
+          Next Page
+        </button>
+      </div>
+
+      <div className="grid grid-rows-3 grid-cols-4 gap-5 mx-10">
+        
         {storage.map((person) => {
-          return <div key={person.id} className='flex gap-2 bg-purple-600 rounded-md'>
+          return <div key={nanoid()} className='flex gap-2 bg-purple-600 rounded-md'>
             <div className="p-4">
               <p>Name: {person.name}</p>
               <p>Status: {person.status}</p>
